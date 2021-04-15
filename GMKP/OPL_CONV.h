a2e07a57177d8e4977ddbef704088ee9b57a96f0
dariodenardi <dario.denardi@outlook.com>
2021-04-15 10:21:31 +0200

    small fixes

---
+++ GMKP/OPL_CONV.h
@@ -0,0 +1,12 @@
+#ifndef OPL_CONV_H_
+#define OPL_CONV_H_
+
+#include <iostream>
+#include <fstream>
+#include <sstream>
+
+#include <iomanip>
+
+int convertLpToMod(int n, int m, int r, int * b, int * weights, int * profits, int * capacities, int * setups, int * classes, int * indexes, char * modFilename);
+
+#endif /* OPL_CONV_H_ */
